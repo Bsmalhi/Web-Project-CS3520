203626de728aca62da9fdb3fe6fb6c47338da681
Wei Su <willsuwei@hotmail.com>
2016-05-24 19:33:00 -0700

    auto

--- WebProject_CS3520/src/java/com/willsuwei/cs3520/SendEmail.java
+++ WebProject_CS3520/src/java/com/willsuwei/cs3520/SendEmail.java
@@ -54,63 +54,33 @@ public class SendEmail extends HttpServlet {
     public static void sendEmail(String email, String message){
         System.out.println("Email: " + email + "---" + "Message: " + message);
         try{
-           Send("com.willsuwei.cs3520", "cs3520admin", email, "", "Snap Chat", message);
-       } catch(Exception e){
-           System.out.println(e);
-           e.printStackTrace();
-       }
-   }
-    
-   public static void Send(final String username, final String password, String recipientEmail, String ccEmail, String title, String message) throws AddressException, MessagingException {
-        Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
-        final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
+            String username = "com.willsuwei.cs3520@gmail.com";
+            String password = "cs3520admin";
+            String title = "Snap Chat";
 
-        // Get a Properties object
-        Properties props = System.getProperties();
-        props.setProperty("mail.smtps.host", "smtp.gmail.com");
-        props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
-        props.setProperty("mail.smtp.socketFactory.fallback", "false");
-        props.setProperty("mail.smtp.port", "465");
-        props.setProperty("mail.smtp.socketFactory.port", "465");
-        props.setProperty("mail.smtps.auth", "true");
+            Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+            final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
+            Properties props = System.getProperties();
+            props.setProperty("mail.smtps.host", "smtp.gmail.com");
+            props.setProperty("mail.smtps.auth", "true");
+            props.setProperty("mail.smtp.socketFactory.fallback", "false");
+            props.setProperty("mail.smtp.port", "465");
+            props.setProperty("mail.smtp.socketFactory.port", "465");
+            props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
+            props.put("mail.smtps.quitwait", "false");
 
-        /*
-        If set to false, the QUIT command is sent and the connection is immediately closed. If set 
-        to true (the default), causes the transport to wait for the response to the QUIT command.
+            Session session = Session.getInstance(props, null);
 
-        ref :   http://java.sun.com/products/javamail/javadocs/com/sun/mail/smtp/package-summary.html
-                http://forum.java.sun.com/thread.jspa?threadID=5205249
-                smtpsend.java - demo program from javamail
-        */
-        props.put("mail.smtps.quitwait", "false");
-
-        Session session = Session.getInstance(props, null);
-
-        // -- Create a new message --
-        final MimeMessage msg = new MimeMessage(session);
-
-        // -- Set the FROM and TO fields --
-        msg.setFrom(new InternetAddress(username + "@gmail.com"));
-        msg.setRecipients(javax.mail.Message.RecipientType.TO, InternetAddress.parse(recipientEmail, false));
-
-        if (ccEmail.length() > 0) {
-            msg.setRecipients(javax.mail.Message.RecipientType.CC, InternetAddress.parse(ccEmail, false));
-        }
-
-        msg.setSubject(title);
-        msg.setText(message, "utf-8");
-        msg.setSentDate(new Date());
-
-        SMTPTransport t = (SMTPTransport)session.getTransport("smtps");
-
-        t.connect("smtp.gmail.com", username, password);
-        t.sendMessage(msg, msg.getAllRecipients());      
-        t.close();
-    }
-    
-   public static void main(String [] args){
-       try{
-           Send("com.willsuwei.cs3520", "cs3520admin", "willsuwei@hotmail.com", "", "testmessageTitle", "Test message context");
+            final MimeMessage msg = new MimeMessage(session);
+            msg.setFrom(new InternetAddress(username));
+            msg.setRecipients(javax.mail.Message.RecipientType.TO, InternetAddress.parse(email, false));
+            msg.setSubject(title);
+            msg.setText(message, "utf-8");
+            msg.setSentDate(new Date());
+            SMTPTransport t = (SMTPTransport)session.getTransport("smtps");
+            t.connect("smtp.gmail.com", username, password);
+            t.sendMessage(msg, msg.getAllRecipients());      
+            t.close();
        } catch(Exception e){
            System.out.println(e);
            e.printStackTrace();
--- WebProject_CS3520/src/java/com/willsuwei/cs3520/SignInAdmin.java
+++ WebProject_CS3520/src/java/com/willsuwei/cs3520/SignInAdmin.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package AdminLogin;
+package com.willsuwei.cs3520;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
